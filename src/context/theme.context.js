97e39600a33f2c739fcff086e9bdb9c966b6f11a
yebrai <ivansevv@gmail.com>
2022-11-07 18:10:46 +0100

    todo client fixes

--- src/context/theme.context.js
+++ src/context/theme.context.js
@@ -1,61 +1,53 @@
-import { createContext, useState } from 'react'
+import { createContext, useState } from "react";
 
-const ThemeContext = createContext()
-//es un Componentes, lo crea react con createContext() 
+const ThemeContext = createContext();
+//es un Componentes, lo crea react con createContext()
 
 function ThemeWrapper(props) {
+  //creamos todos los estados y funciones que queremos pasar por contexto
+  const [darkMode, setDarkMode] = useState(false);
 
-//creamos todos los estados y funciones que queremos pasar por contexto
-const [darkMode, setDarkMode] = useState(false)
+  const darkTheme = {
+    backgroundColor: "black",
+    color: "darkGray",
+  };
 
-const darkTheme = {
-  backgroundColor: "black",
-  color: "darkGray"
+  const lightTheme = {
+    backgroundColor: "white",
+    color: "black",
+  };
+
+  const darkThemeBtn = {
+    backgroundColor: "red",
+  };
+
+  const lighThemeBtn = {
+    backgorundColor: "lightblue",
+  };
+
+  const switchThemeBtn = () => {
+    return darkMode ? darkThemeBtn : lighThemeBtn;
+  };
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const switchTheme = () => {
+    return !darkMode ? lightTheme : darkTheme;
+  };
+
+  const passedContext = {
+    switchThemeBtn,
+    toggleTheme,
+    switchTheme,
+  };
+
+  return (
+    <ThemeContext.Provider value={passedContext}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
 }
 
-const lightTheme = {
-  backgroundColor: "white",
-  color: "black"
-}
-
-const darkThemeBtn = {
-  backgroundColor:"red"
-}
-
-const lighThemeBtn = {
-  backgorundColor:"lightblue"
-}
-
-const switchThemeBtn = () => {
-  return darkMode ? darkThemeBtn : lighThemeBtn
-}
-
-const toggleTheme = () => {
-  setDarkMode(!darkMode)
-}
-
-const switchTheme = () => {
-  return !darkMode ? lightTheme : darkTheme
-}
-
-
-    const passedContext = {
-        switchThemeBtn,
-        toggleTheme,
-        switchTheme
-
-    }
-
-
-    return (
-        <ThemeContext.Provider value={passedContext}>
-            {props.children}
-        </ThemeContext.Provider>
-    )
-
-}
-
-export {
-    ThemeWrapper,
-    ThemeContext 
-}
+export { ThemeWrapper, ThemeContext };
